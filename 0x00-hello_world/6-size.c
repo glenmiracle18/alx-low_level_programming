21d360ff079bf408e32bc07494beb7474980fc29
glenmiracle18 <glenmiracle67@gmail.com>
2022-10-13 15:37:45 -0700

    Print sizes of variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio..h>
+/**
+ * main - Entry point/
+ *
+ * Returnn : 0 always successful.
+ */
+
+int main(void)
+{
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of int: %dbyte(s)\n", sizeof(int));
+	pritnf("Size of long int: %d bytes(s)", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+
