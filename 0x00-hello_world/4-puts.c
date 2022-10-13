9c69404874fbd273ce29549a8f680d8bdc439f90
glenmiracle18 <glenmiracle67@gmail.com>
2022-10-13 14:33:35 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* *
+ * Description: main - prints out all sdout.
+ * Return: 0 if succesful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
