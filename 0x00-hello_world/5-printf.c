ff392b56e1a16936bf306f04f212ce493fe2c833
glenmiracle18 <glenmiracle67@gmail.com>
2022-10-13 15:09:00 -0700

    Using printf functions

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * maiun - prints a function to stdout.
+ * Return : 0 if succesful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
