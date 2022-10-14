ce18d89bd41687e989a3bbb0e9f1691d87035404
glenmiracle18 <glenmiracle67@gmail.com>
2022-10-14 16:12:37 -0700

    Alphabet with reverse

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+
+
+/**
+ * main-program entry
+ * 
+ * Return>0 if no error, non error
+ */
+
+
+int main(void)
+
+{
+
+	int lw = 'a';
+
+	int up = 'A';
+
+	while (lw <= 'z')
+
+	{
+
+		putchar(lw);
+
+		lw += 1;
+
+	}
+
+	while (up <= 'Z')
+
+	{
+
+		putchar(up);
+
+		up += 1;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
