cd4a50a702a8a34feb5d2c2a54898bd4cc07af75
glenmiracle18 <glenmiracle67@gmail.com>
2022-10-14 16:38:54 -0700

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ *
+ * main-program entry poing
+ *
+ * Return:0 If no more errow
+ */
+
+
+
+int main(void)
+
+{
+
+	int lw = 'a';
+
+	while (lw <= 'z')
+
+	{
+
+		if (lw == 'e' || lw == 'q')
+
+		{
+
+			lw += 1;
+
+		}
+
+		else
+
+		{
+
+			putchar(lw);
+
+			lw += 1;
+
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
