5317933841cde3a7be18ffde5f910b8cb0b44032
glenmiracle18 <glenmiracle67@gmail.com>
2022-10-15 12:57:17 -0700

    print single digits base 10 numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* 
+ * main - prints all single digit base10 numbers.
+ *Return: 0 returns the valueand ends the program
+ *
+ */
+
+
+
+int main(void)
+
+{
+
+	int n = 48;
+
+	while (n <= 57)
+
+	{
+
+		putchar(n);
+
+		n += 1;
+
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
