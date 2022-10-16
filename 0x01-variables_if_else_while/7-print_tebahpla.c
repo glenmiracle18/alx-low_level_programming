7fe840cd2414f2a081796b2c5f08b580a50f58e5
glenmiracle18 <glenmiracle67@gmail.com>
2022-10-16 09:40:09 -0700

    letter of the alphabet in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Starts or begining of the program
+ * Return: 0 Ends the program
+ */
+
+
+
+int main(void)
+
+{
+
+	int n = 122;
+
+	while (n > 96)
+
+	{
+
+		putchar(n);
+
+		n -= 1;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
