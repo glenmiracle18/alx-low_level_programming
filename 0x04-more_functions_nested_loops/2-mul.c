b221902c6e3356bbd3077ff4bc357e58dc7de36d
glenmiracle18 <glenmiracle67@gmail.com>
2022-10-21 12:27:55 -0700

    multiplies two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Funtion that acccepts two arguments
+ * @a: first argument
+ * @b: Second argument
+ * Return: multiplied value
+ */
+
+int mul(int a, int b);
+{
+	return (a * b);
+}
