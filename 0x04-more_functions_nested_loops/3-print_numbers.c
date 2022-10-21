476c0d835b01bf12437b1be745d3c3c525edad68
glenmiracle18 <glenmiracle67@gmail.com>
2022-10-21 14:25:47 -0700

    zzero to nine

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,17 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * print_numbers - Function that loops through digits
- * _putchar - Prints characters
- * Return: Returns 0
- */
-
-void print_numbers(void);
+* print_numbers - Function that loops through digits
+* _putchar - Prints characters
+* Return: Returns 0
+*/
+void print_numbers(void)
 {
-	char x = 48;
-	while (x >= 48 && x <= 57)
+	char num = 48;
 
+	while (num >= 48 && num <= 57)
 	{
-		putchar(x);
-		x++;
+		_putchar(num);
+		num++;
 	}
-	putchar('\n');
+	_putchar('\n');
 }
