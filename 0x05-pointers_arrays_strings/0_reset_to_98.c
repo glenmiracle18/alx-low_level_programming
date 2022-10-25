63f5048ba67cdad5bab27d349f8aecd1b837ecd9
glenmiracle18 <glenmiracle67@gmail.com>
2022-10-24 21:39:08 -0700

    verifying

---
+++ 0x05-pointers_arrays_strings/0_reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - updates the value of a parater
+ * through a pointer
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
