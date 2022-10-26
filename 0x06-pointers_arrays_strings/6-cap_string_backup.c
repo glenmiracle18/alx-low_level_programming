0b4322665ef681fe8a892e1c8749078ae9dc591c
glenmiracle18 <glenmiracle67@gmail.com>
2022-10-26 03:58:55 -0700

    done

---
+++ 0x06-pointers_arrays_strings/6-cap_string_backup.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * cap_string - function that capitalizes all words
+ * @ch: Pointer to char
+ * Return: char
+ */
+
+char *cap_string(char *ch)
+{
+	/* will work on this soon */
+}
