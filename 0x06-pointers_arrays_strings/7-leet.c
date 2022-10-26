4f4aa391e762e29f077452ca8d8b34c8e199d069
glenmiracle18 <glenmiracle67@gmail.com>
2022-10-26 09:28:33 -0700

    This was tough

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,16 +2,18 @@
 
 /**
  * leet - function that encodes a string into 1337.
- * @s: pointer to char
- *
- * Return: the pointer dest
+ * @s: A pointer pointint to address of a string to encode
+ * Return: Returns pointer address to char 5
  */
 
 char *leet(char *s)
 {
 	int c = 0;
+
 	int d = 0;
+
 	char a[] = "aAeEoOtTlL";
+
 	char b[] = "4433007711";
 
 	while (*(s + c))
