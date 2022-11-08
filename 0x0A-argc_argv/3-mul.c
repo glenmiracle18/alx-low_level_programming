27c4c704b2b4019983e19c7d3083b0b3ac978bed
glenmiracle18 <glenmiracle67@gmail.com>
2022-11-08 11:15:36 -0800

    new

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,17 +10,17 @@
 
 int main(int argc, char **argv)
 {
-	int result, num1, num2;
+	int num1, num2;
 
-	if (argc != 3)
+	if (argc < 3)
 	{
-		printf("%s\n", "Error");
+		prinf("Error\n");
 		return (1);
 	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	result = num1 * num2;
-	printf("%d\n", result);
+	printf("%d\n", num1 * num2);
+
 	return (0);
 }
