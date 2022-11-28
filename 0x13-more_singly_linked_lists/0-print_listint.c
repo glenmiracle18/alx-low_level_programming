30ae058178d47cde6a7cb73531e5543249e3b8c4
glenmiracle18 <glenmiracle67@gmail.com>
2022-11-28 11:35:53 -0800

    done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,9 +8,9 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t nodes;
-	
+
 	nodes = 0;
-	
+
 	while (h)
 	{
 		printf("%d\n", h->n);
@@ -19,3 +19,4 @@ size_t print_listint(const listint_t *h)
 	}
 	return (nodes);
 }
+
