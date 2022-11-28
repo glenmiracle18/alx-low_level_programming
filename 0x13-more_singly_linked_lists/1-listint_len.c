b7121b286f4fc0e03f8b632bc436176aeae4f679
glenmiracle18 <glenmiracle67@gmail.com>
2022-11-28 11:49:45 -0800

    finished

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,13 +1,20 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked list
+ * listint_len - counts the number of nodes in a linked list
  * @h: head of the list
- * Return: number of elements in the list
+ *
+ * Return: the number of elements
  */
 size_t listint_len(const listint_t *h)
 {
-	if (h == (void *)0)
-		return (0);
-	return (1 + listint_len(h->next));
-}
+	const listint_t *cursor = h;
+	size_t count = 0;
+
+	while (cursor != NULL)
+	{
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
+}i
--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -1,26 +1,36 @@
-#ifndef MORELINKEDLISTS
-#define MORELINKEDLISTS
+#ifndef _LISTS_
+#define _LISTS_
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 
 /**
-* struct listint_s - singly linked list
-* @n: integer
-* @next: points to the next node
-*
-* Description: singly linked list node structure
-* for Holberton project
-*/
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
